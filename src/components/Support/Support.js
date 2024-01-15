f45de7d286c65e8f7d43977c299ee6ccb03a6413
Sri <sriar.srivati@ansira.com>
2024-01-15 12:46:52 -0800

    social icon fix

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -201,9 +201,9 @@ function Support() {
           src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3945.6093161864446!2d76.93912127501312!3d8.537255291505753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMzInMTQuMSJOIDc2wrA1NiczMC4xIkU!5e0!3m2!1sen!2sin!4v1704818994185!5m2!1sen!2sin"
           frameBorder="0"
           style={{ border: 0 }}
-          allowfullscreen=""
+          allowFullScreen=""
           aria-hidden="false"
-          tabindex="0"
+          tabIndex="0"
         ></iframe>
       </div>
       <Contact />
